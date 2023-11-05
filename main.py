2815c759f15e4e4910958436bda7f30ad70435e4
Keyel <keyel@localhost.localdomain>
2023-11-05 15:13:05 -0600

    [Update] Main

--- main.py
+++ main.py
@@ -1,25 +1,65 @@
 import sys
+import re
 
-def tokenizar(codigo):
-    tokens = ['class', 'func', 'main', 'while', 'do', 'for', 'range', 'true', 'false', ' if', 'else', 'elsif', 'switch', '(', ')', '{', '}', 'case', 'default',
-              'exit', 'in', 'sqrt', 'print', 'println', '+', '-', '*', '/', '**', '[', ']', '=', '==', '>=', '<=', '>', '<', '!=', '>>', 'and', 'or', '&&', '||',
-              '!', 'not', '+=', '-=', '*=', '/=', '%', '%=', '+=', '-=', '++', '--', '//']
-    token_list = []
-    lineas = codigo.split('\n')
-    i = 1
-    for linea in lineas:
-        token_list_linea = []
-        palabras = linea.split()
-        for palabra in palabras:
-            if palabra in tokens:
-                token_list_linea.append(palabra)
-        if token_list_linea.__len__() > 0:
-            token_list.append((i, token_list_linea))
-        i += 1
-    return token_list
+# Definir los tipos de tokens
+TOKENS = [
+    ('KEYWORD', r'\b(class|func|if|else|elsif|switch|case|default|exit|while|do|for|range|in|sqrt|println|print)\b'),
+    ('IDENTIFIER', r'\b[a-zA-Z][a-zA-Z0-9]*\b'),
+    ('NUMBER', r'\b\d+(\.\d*)?\b'),
+    ('STRING', r'"[^"]*"'),
+    ('OPERATOR', r'==|!=|>=|<=|='),
+    ('PUNCTUATION', r'[{}(),.]'),
+    ('WHITESPACE', r'\s+'),
+    ('UNKNOWN', r'.')
+]
+
+# Definir la gramática
+GRAMMAR = {
+    'program': [['statement', 'program'], ['statement']],
+    'statement': [['KEYWORD', 'IDENTIFIER', 'PUNCTUATION', 'expression', 'PUNCTUATION']],
+    'expression': [['IDENTIFIER'], ['NUMBER'], ['STRING'], ['expression', 'OPERATOR', 'expression']]
+}
 
 def analizar_codigo(codigo):
-    return tokenizar(codigo)
+    tokens = []
+    while codigo:
+        match = None
+        for token_type, pattern in TOKENS:
+            regex = re.compile(pattern)
+            match = regex.match(codigo)
+            if match:
+                value = match.group(0)
+                if token_type != 'WHITESPACE':
+                    tokens.append((token_type, value))
+                codigo = codigo[match.end():]
+                break
+        if not match:
+            sys.stderr.write('Error: Invalid character\n')
+            sys.exit(1)
+    return tokens
+
+def validar_tokens(tokens):
+    stack = [['program']]
+    for token_type, value in tokens:
+        while True:
+            if not stack:
+                return False
+            top = stack[-1]
+            if isinstance(top, list):
+                if not top:
+                    stack.pop()
+                    continue
+                next_symbol = top[0]
+                if next_symbol in GRAMMAR:
+                    stack.append(GRAMMAR[next_symbol].pop(0))
+                elif next_symbol == token_type:
+                    top.pop(0)
+                    break
+                else:
+                    return False
+            else:
+                stack.pop()
+    return not stack
 
 if len(sys.argv) != 2:
     print("Use: zaphyr <archivo.zph>")
@@ -28,9 +68,9 @@ else:
     ruta_archivo = sys.argv[1]
     with open(ruta_archivo, 'r') as archivo:
         codigo = archivo.read()
-        token_lines=analizar_codigo(codigo)
-        for tokens in token_lines:
-            tokens = tokens[1]
-            for token in tokens:
-                print(token)
-                
+        tokens = analizar_codigo(codigo)
+        if validar_tokens(tokens):
+            print("El código es válido.")
+        else:
+            print("El código no es válido.")
+            sys.exit(1)
