be8dd06ef85636da5d92a586faf54d744f9d2eb5
Luis <luis@localhost.localdomain>
2023-11-06 15:34:29 -0600

    [ADD] logica

--- main.py
+++ main.py
@@ -2,82 +2,44 @@ from lark import Lark, exceptions
 import tkinter as tk
 
 grammar = """
-?start: stament
+?start: program
 
-content_class: declaration
-      | function
-      | main_func
+?program:class_declaration | class_main
 
-stament:main_class 
-      | class_def
-      | declaration
-      | loop
-      | conditional
-      | print_data
-      | input_data
-      | square_root
+?class_main: "class" "main" "{" class_body "}"
+?class_declaration: "class" IDENTIFIER "{" class_body "}"
 
-?declaration: sv
-sv: (IDENTIFIER ","*)+ EQ (complex_arg ","*)+
-var_list: LETTER (COMMA LETTER rdv?)*
 IDENTIFIER: LETTER+
 
-complex_arg: STRING | number | BOOLEAN | var_value
+class_body: var_declaration
+            | func_def
+            | func_main
 
-?function: sf
-sf: "func" func_name "{" content "}"
-func_name: IDENTIFIER "(" param_list ")" | IDENTIFIER "(" ")"
-param_list: IDENTIFIER (COMMA param_list)?
+func_def: "func" IDENTIFIER "{" body "}"
+func_main: "func" "main" "{" body "}"
 
-?class_def: sc
-sc: "class" class_name "{" content_class "}"
-class_name: LETTER rdv?
+body: var_declaration
+    | ciclo
+    | condicional
+    | print
+    | in
+    | sqrt
 
-?loop: sl
-sl: "while" "(" condition ")" "{" content "}"
-   | "do" "{" content "}" "while" "(" condition ")"
-   | "for" loop_config
-loop_config: LETTER rdv? "range" "(" range_arg ")"
-range_arg: number | LETTER rdv?
 
-?conditional: sco
-sco: "if" "(" condition ")" "{" content "}" ("elsif" "(" condition ")" "{" content "}")* ("else" "{" content "}")?
-    | "switch" "{" case_action "}"
-condition: var_value | condition_var
-condition_var: var_value EQ_OP var_value | var_value EQ_OP complex_arg
-EQ_OP: "==" | "!=" | ">=" | "<=" | ">" | "<"
-default_arg: STRING | BOOLEAN | number
-case_action: case+ default?
-case:"case" "==" (number | STRING | IDENTIFIER) "{" content "}"
-default: "default" "{" "exit" "}"
-
-?main_func: sfm
-sfm: "func main() {" content "}"
-
-?main_class: scm
-scm: "class main {" content_class "}"
-
-?print_data: sp
-sp: "println" "(" complex_arg ")" | "print" "(" complex_arg ")"
-
-var_value: LETTER rdv? 
-
-?input_data: si
-si: "in" ">>" var_value
-
-?square_root: sq
-sq: "sqrt" "(" number ")" | "sqrt" "(" var_value ")"
-
-LETTER: "a".."z" | "A".."Z"
-number: (DIGIT)+ | (DIGIT)+ POINT (DIGIT)+
-rdv: LETTER rdv?
+print: "print" "(" STRING ")" | "println" "(" STRING ")"
+in: "in" ">>" IDENTIFIER
+sqrt: "sqrt" "(" NUM ")" | "sqrt" "(" NUM ")"
+ciclo: "while" "(" condition ")" "{" body "}" | "do" "{" body "}" "while" "(" condition ")" | "for" IDENTIFIER "range""("IDENTIFIER")""{"body"}"
+condicional: "if" "(" condition ")" "{" body "}" ("elsif" "(" condition ")" "{" body "}")* ("else" "{" body "}")? | "switch""("IDENTIFIER")""{"switch_body"}"
+condition: IDENTIFIER "==" IDENTIFIER | IDENTIFIER "!=" IDENTIFIER | IDENTIFIER ">" IDENTIFIER | IDENTIFIER "<" IDENTIFIER | IDENTIFIER ">=" IDENTIFIER | IDENTIFIER "<=" IDENTIFIER
+var_declaration: (IDENTIFIER | ",")+ "=" (NUM | STRING | BOOLEAN | ",")+
+switch_body: "case" "==" (NUM | STRING) "{"body "}" | "default" "{" "exit" "}"
+STRING: /"(\\"|[^"])*"/
 DIGIT: "0".."9"
 POINT: "."
-EQ: "="
-COMMA: ","
+NUM: DIGIT+ | DIGIT+ POINT DIGIT+
+LETTER: "a".."z" | "A".."Z"
 BOOLEAN: "true" | "false"
-STRING: /"(\\"|[^"])*"/
-content: (function | declaration | loop | conditional | main_func | print_data | input_data | square_root)*
 
 %import common.WS
 %ignore WS
@@ -89,7 +51,7 @@ def evaluar_cadena():
     try:
         input_text = entrada_texto.get("1.0", tk.END)
         parsed = parser.parse(input_text)
-        print(parsed.pretty())
+        print(parsed)
     except exceptions.LarkError as e:
         print(f"Error: {e}")
 
