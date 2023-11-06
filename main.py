667aefd2e0f5790aca86689cc3bbc75645dc4e02
Luis <luis@localhost.localdomain>
2023-11-06 14:42:45 -0600

    [ADD] gramatica

--- main.py
+++ main.py
@@ -1,208 +1,105 @@
-import os
-import re
-import sys
+from lark import Lark, exceptions
+import tkinter as tk
 
-error = False
-line_number = 0
-nombres_declarados = set()
+grammar = """
+?start: stament
 
-gramatica = {
-    'L': r'[a-zA-Z]+',
-    'LB': r'{',
-    'RB': r'}',
-    'LP': r'\(',
-    'OS': r'"|"',
-    'N': r'[0-9]+',
-    'CO': r'\,',
-    'P': r'\.',
-    'E': r'=',
-    'B': r'(true|false)',
-    'F': r'^(func)',
-    'C': r'^(class)',
-    'CC': r'^(while|do|for)',
-    'O': r'==|!=|>=|<=|+|-|*|/|**',
-    'RC': r'(range)',
-    'SC': r'^(if|elsif|else|switch)',
-    'CA': r'^(case|default)',
-    'EX': r'^(exit)$',
-    'M': r'(main)',
-    'RP': r'^(print|println)',
-    'RI': r'^(in)',
-    'OI': r'(>>)',
-    'RR': r'^(return)'
-}
+content_class: declaration
+      | function
+      | main_func
 
-regex_identifiers = fr'({gramatica["L"]})(\s*,\s*{gramatica["L"]})*'
+stament:main_class 
+      | class_def
+      | declaration
+      | loop
+      | conditional
+      | print_data
+      | input_data
+      | square_root
 
-regex_number = fr'({gramatica["N"]}{gramatica["P"]}{gramatica["N"]})|({gramatica["N"]})'
+?declaration: sv
+sv: (IDENTIFIER ","*)+ EQ (complex_arg ","*)+
+var_list: LETTER (COMMA LETTER rdv?)*
+IDENTIFIER: LETTER+
 
-regex_string = r'("([^"\\]|\\.)*")'
+complex_arg: STRING | number | BOOLEAN | var_value
 
-regex_valor = ( fr'({gramatica["L"]}|{regex_number}|{gramatica["B"]})|({gramatica["L"]}|{regex_number}|{gramatica["B"]}|{regex_string}){gramatica["CO"]}')
+?function: sf
+sf: "func" func_name "{" content "}"
+func_name: IDENTIFIER "(" param_list ")" | IDENTIFIER "(" ")"
+param_list: IDENTIFIER (COMMA param_list)?
 
-regex_sv = ( fr'\s*({regex_identifiers})\s*' fr'\s*({gramatica["E"]})\s*' fr'\s*({regex_valor})\s*' )
+?class_def: sc
+sc: "class" class_name "{" content_class "}"
+class_name: LETTER rdv?
 
-regex_data_print = (fr'({regex_string}|{gramatica["L"]}|\s+|\+)+')
+?loop: sl
+sl: "while" "(" condition ")" "{" content "}"
+   | "do" "{" content "}" "while" "(" condition ")"
+   | "for" loop_config
+loop_config: LETTER rdv? "range" "(" range_arg ")"
+range_arg: number | LETTER rdv?
 
-regex_print = fr'\s*({gramatica["RP"].lstrip("^")})\s*{gramatica["LP"]}{regex_data_print}\)\s*'
+?conditional: sco
+sco: "if" "(" condition ")" "{" content "}" ("elsif" "(" condition ")" "{" content "}")* ("else" "{" content "}")?
+    | "switch" "{" case_action "}"
+condition: var_value | condition_var
+condition_var: var_value EQ_OP var_value | var_value EQ_OP complex_arg
+EQ_OP: "==" | "!=" | ">=" | "<=" | ">" | "<"
+default_arg: STRING | BOOLEAN | number
+case_action: case+ default?
+case:"case" "==" (number | STRING | IDENTIFIER) "{" content "}"
+default: "default" "{" "exit" "}"
 
-regex_input = fr'\s*({gramatica["RI"].lstrip("^")}\s*{gramatica["OI"]}\s*{gramatica["L"]})'
+?main_func: sfm
+sfm: "func main() {" content "}"
 
-variables = []
+?main_class: scm
+scm: "class main {" content_class "}"
 
-def error_message(i, missing):
-    print('Syntax error: Se esperaba "'+ missing + '" en la linea ' + str(i+1))
-    exit(1)
+?print_data: sp
+sp: "println" "(" complex_arg ")" | "print" "(" complex_arg ")"
 
-def error_unrecognize(i, u):
-    print('Syntax error: "'+ u + '" no se reconoce en la linea ' + str(i+1))
-    exit(1)
+var_value: LETTER rdv? 
 
-def validar_print(tokens, i):
-    print(tokens)
-    if re.match(gramatica['LP'], tokens[1]):
-        print(re.match(regex_string, "cadena"))#tokens[1:-2]):
-    else:
-        error_message(i, '(')
+?input_data: si
+si: "in" ">>" var_value
 
-def validar_declaracion_variables(tokens, l):
-    lenList = len(tokens)
-    mid_pos = (lenList)//2
-    i = 0
-    tokens = [token.replace(',', '') for token in tokens]
-    if lenList % 2 != 0:
-        if re.match(gramatica["E"], tokens[mid_pos]):
-            while i < mid_pos:
-                if not re.match(gramatica["L"], tokens[i]):
-                    error_message (l, "identificador")
-                else:
-                    if not tokens[i] in variables:
-                        variables.append(tokens[i])
-                    else:
-                        error_message (l, "variable nueva")
-                i += 1
-            i += 1
-            while i < len(tokens):
-                if not re.match(regex_valor, tokens[i]):
-                    error_message (l, "valor")
-                i += 1
-        else:
-            error_message (l, "= en el medio")
-    else:
-        error_message (l, "= en el medio")
-    return l + 1
+?square_root: sq
+sq: "sqrt" "(" number ")" | "sqrt" "(" var_value ")"
 
-def validar_cuerpo_funcion(lineas, i):
-    linea = lineas[i]
-    if linea.strip() == '':
-        i = validar_cuerpo_funcion(lineas, i+1)
-    else:
-        tokens = linea.split()
-        if re.match(regex_sv, lineas[i]):
-            i = validar_declaracion_variables(tokens, i)
-            i = validar_cuerpo_funcion(lineas, i)
-        elif re.match(regex_print, lineas[i]):
-            i += 1
-            i = validar_cuerpo_funcion(lineas, i)
-        elif re.match(regex_input, lineas[i]):
-            i += 1
-            i = validar_cuerpo_funcion(lineas, i)
-    return i
+LETTER: "a".."z" | "A".."Z"
+number: (DIGIT)+ | (DIGIT)+ POINT (DIGIT)+
+rdv: LETTER rdv?
+DIGIT: "0".."9"
+POINT: "."
+EQ: "="
+COMMA: ","
+BOOLEAN: "true" | "false"
+STRING: /"(\\"|[^"])*"/
+content: (function | declaration | loop | conditional | main_func | print_data | input_data | square_root)*
 
-def validar_funciones(lineas, i):
-    tokens = re.findall(r'\,|\(|\)|\{|\w+', lineas[i])
-    if re.match(gramatica['F'], tokens[0]):
-        if re.match(gramatica['M'], tokens[1]):
-            if re.match(gramatica['LP'], tokens[2]):
-                if ")" == tokens[3]:
-                    if re.match(gramatica['LB'], tokens[4]):
-                        i = validar_cuerpo_funcion(lineas, i+1)
-                        if not re.match(gramatica['RB'], lineas[i]):
-                            error_message(i, '}')
-                    else:
-                        error_message(i, '{')
-                else:
-                    error_message(i, ')')
-            else:
-                error_message(i, '(')
-        elif re.match(gramatica['L'], tokens[1]):
-            if re.match(gramatica['LP'], tokens[2]):
-                j = 3
-                while j < len(tokens) and tokens[j] != ')':
-                    if j%2 != 0:
-                        if not re.match(gramatica["L"], tokens[j]):
-                            error_message (i, "identificador")
-                    else:
-                        if ',' != tokens[j]:
-                            error_message (i, ",")
-                    j += 1
-                if ")" == tokens[j]:
-                    if j+1 < len(tokens) and re.match(gramatica['LB'], tokens[j+1]):
-                        i = validar_cuerpo_funcion(lineas, i+1)
-                        if not re.match(gramatica['RB'], lineas[i]):
-                            error_message(i, '}')
-                    else:
-                        error_message(i, '{')
-                else:
-                    error_message(i, ')')
-            else:
-                error_message(i, '(')
-        else:
-            error_message(i, 'identificador')
-    else:
-        error_message(i, 'func')
-    return i
+%import common.WS
+%ignore WS
+"""
 
-def validar_cuerpo_class(lineas, i):
-    linea = lineas[i]
-    if linea.strip() == '':
-        i = validar_cuerpo_class(lineas, i+1)
-    else:
-        tokens = re.findall(r'\(|\)|\{|\w+', linea)
-        if re.match(regex_sv, lineas[i]):
-            tokensV = linea.split()
-            i = validar_declaracion_variables(tokensV, i)
-            i = validar_cuerpo_class(lineas, i)
-        elif re.match(gramatica["F"], tokens[0]):
-            i = validar_funciones(lineas, i)
-            i = validar_cuerpo_class(lineas, i)
-    return i
+parser = Lark(grammar, start='start')
 
-def validar_classes(lineas, i):
-    linea = lineas[i]
-    if linea == '':
-        validar_classes(lineas, i+1)
-    else:
-        tokens = linea.split(' ')
-        if re.match(gramatica['C'], tokens[0]):
-            if re.match(gramatica['L'], tokens[1]) or re.match(gramatica['M'], tokens[1]):
-                if re.match(gramatica['LB'], tokens[2]):
-                    i = validar_cuerpo_class(lineas, i+1)
-                    if not re.match(gramatica['RB'], lineas[i]):
-                        error_message(i, '}')
-                else:
-                    error_message(i, '{')
-            else:
-                error_message(i, 'nombre de clase')
-        else:
-            error_message(i, 'class')
+def evaluar_cadena():
+    try:
+        input_text = entrada_texto.get("1.0", tk.END)
+        parsed = parser.parse(input_text)
+        print(parsed.pretty())
+    except exceptions.LarkError as e:
+        print(f"Error: {e}")
 
+root = tk.Tk()
+root.title("Lyra: Analizador Sintáctico")
 
-def validar_codigo(codigo):
-    lineas = codigo.split('\n')
-    validar_classes(lineas, 0)
+entrada_texto = tk.Text(root, height=15, width=60)
+entrada_texto.pack()
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Use: zaphyr <archivo.zph>")
-        sys.exit(1)
-    else:
-        ruta_archivo = sys.argv[1]
-        _, extension_archivo = os.path.splitext(ruta_archivo)
-        if extension_archivo != ".zph":
-            print("El archivo debe ser .zph")
-            sys.exit(1)
-        
-        with open(ruta_archivo, 'r') as archivo:
-            codigo = archivo.read()
-            validar_codigo(codigo)
+boton_evaluar = tk.Button(root, text="Evaluar cadena", command=evaluar_cadena)
+boton_evaluar.pack()
+
+root.mainloop()
